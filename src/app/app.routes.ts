67ec2fa5d308a164d1bed552432eae818ebc2e12
Never Lozano <nlozanob@ufpso.edu.co>
2025-03-27 20:44:37 -0500

    AS2-9 Protecion de rutas NLB

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { LoginComponent } from './components/login/login.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component:  WelcomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent},
-  { path: 'dashboard', component: DashboardComponent},
   { path: '**', redirectTo: '' }, // Redirige rutas desconocidas a Welcome
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
 ];
