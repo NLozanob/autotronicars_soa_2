b938089ae0a8bc5978d587fa5c01137323c7fc2e
Never Lozano <nlozanob@ufpso.edu.co>
2025-03-28 19:49:37 -0500

    Solucionando incovenientes en el proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,13 @@ import { LoginComponent } from './components/login/login.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-<<<<<<< Updated upstream
-=======
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { AuthGuard } from './guards/auth.guard';
-
->>>>>>> Stashed changes
 
 export const routes: Routes = [
   { path: '', component:  WelcomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent},
   { path: 'dashboard', component: DashboardComponent},
+  { path: 'forgot-password', component: ForgotPasswordComponent},
   { path: '**', redirectTo: '' }, // Redirige rutas desconocidas a Welcome
 ];
